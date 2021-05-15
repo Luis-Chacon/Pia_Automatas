f8be9c888f9cd3bb363bcccecc6de31d66eb0c7c
luis_chacon01 <luis.chacon13@hotmail.com>
2021-05-15 18:20:08 -0500

    Pia_Falta Operaciones

--- Prueba1.cpp
+++ Prueba1.cpp
@@ -1,33 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <ctype.h>
 
-struct datos_programa
-{
-	char nom_programa[20];
-	char iniciar[8];
-	char i[10];
-	int val_i;
-	char j[10];
-	int val_j;
-	char k[10];
-	int val_k;
-	char p[10];
-	int val_p;
-	char q[10];
-	int val_q;
-	char imprimir[15];
-	char terminar[15];
-};
-
+bool val_NomProg(char* linea);
+bool val_iniciar(char* linea);
+bool val_Quees(char* linea);
+bool val_leer(char* linea1);
+bool val_operacion(char* linea1);//Aiuda Lorenzo xd 
+bool val_imprimir(char* linea1);
+bool val_terminar(char* linea1);
 
 int main()
 {
-	char cadena[20];
-	
-	struct datos_programa DP;
+	bool bandera = false;
 	int contador=0;
-	int k=0;
-	int *puntero;
+	char cad[100];
 	
 	FILE *documento = fopen ("Pia_Automatas.txt","r");
 	if(documento == NULL)
@@ -38,82 +27,56 @@ int main()
 		return 0;
 	}
 	
-	char linea[100];
-	//documento.getline(linea,sizeof(linea));
-	while(feof(documento) == NULL)
-	{
 
-		/*switch (contador)
+	while(feof(documento) == 0)
+	{
+		fgets(cad,100,documento);
+
+		if(contador < 2)
 		{
-		
-			case 0: fscanf(documento,"%s",&DP.nom_programa);
-					break;
-			case 1: fscanf(documento,"%s",&DP.iniciar);
-					break;
-			
-			case 2: if(k=0)
-					{
-						fscanf(documento,"%s %d",&DP.i, &DP.val_i);
-						k++;
-					}
-					else if(k == 1)
-					{
-						puntero = strtok(linea, '34')
-						strcpy(DP.val_i,puntero);
-						//fscanf(documento,"%d",&DP.val_i);
-						k=0;
-					}	
-					break;
-			
-			case 3: if(k=0)
-					{
-						fscanf(documento,"%s",&DP.j);
-						k++;
-					}
-					else if(k == 1)
-					{
-						fscanf(documento,"%d",&DP.val_j);
-						k=0;
-					}	
-					break;
-			
-			case 4: if(k=0)
-					{
-						fscanf(documento,"%s",&DP.k);
-						k++;
-					}
-					else if(k == 1)
-					{
-						fscanf(documento,"%d",&DP.val_k);
-						k=0;
-					}	
-					break;
-			case 5: fscanf(documento,"%s",&DP.p);
-					break;
-			case 6: fscanf(documento,"%s",&DP.q);
-					break;
-			case 7: fscanf(documento,"%s",&DP.imprimir);
-					break;
-			case 8: fscanf(documento,"%s",&DP.terminar);
-					break;
+			if(contador == 0)
+			{
+				bandera = val_NomProg(cad);
+				contador++;
+				if(!bandera)
+				{
+					printf("Error en la instruccion programa nombre, ERROR DE SINTAXIS");
+				}
+			}
+			else
+			{
+				bandera = val_iniciar(cad);
+				contador++;
+				if(!bandera)
+				{
+					printf("Error en la instruccion iniciar, ERROR DE SINTAXIS");
+				}
+			}
+		}	
+		else if(contador == 2)		//Validar que minimo haya un leer;
+		{
+			bandera = val_leer(cad);
+			contador++;
+			if(!bandera)
+			{
+					printf("Error en instruccion leer, ERROR DE SINTAXIS");
+			}
 		}
-		contador=contador+1;
-		
-		if(contador == 8)
-		{
-			printf("%s\n"
-		   "%s\n"
-		   "%s %d\n" 
-		   "%s %d\n"
-		   "%s %d\n"
-		   "%s\n"
-		   "%s\n"
-		   "%s\n"
-		   "%s\n",DP.nom_programa,DP.iniciar,DP.i,DP.val_i,DP.j,DP.val_j,DP.k,DP.val_k,DP.p,DP.q,DP.imprimir,DP.terminar);
-		}*/
-		fscanf(documento,"%s", &cadena);
-		printf("\n%s", cadena);
+			else if(contador > 2)
+			{
+				bandera = val_Quees(cad);
+			}
+			
+		if(!bandera)
+			break;
+			
 	}
+	
+	if(bandera)
+		puts("\nPrograma Copilado Correctamente");
+		
+	else
+		puts("\n------ERROR DEL PROGRAMA------");
 	
 	
 	
@@ -122,5 +85,221 @@ int main()
 	puts("\n");
 	system("pause");
 	return 0;
+}
+
+bool val_NomProg(char* linea)
+{
+	char NomProg[] = "programa ";
+	int i,j=0;
+	bool ban = false;
+	
+	for(i=0;i<strlen(linea);i++)
+	{
+		if( i<=8 && NomProg[i] == linea[i])
+		{
+			ban = true;
+			//printf("\n%d",i);
+		}
+		else if(i>8)
+		{
+			if(isalpha(linea[i]) && j != -1)
+			{
+				j++;		
+			}
+			else if( (j > 0 || j == -1) && isdigit(linea[i]))
+			{
+				j = -1;
+				//puts("\nBan Verdadero 2");
+				ban = true;
+			}
+				else if(linea[i] == ';' && linea[i+1]=='\n')
+				
+				{
+					ban = true;
+					//puts("\nBan Verdadero 3");
+					break;
+				}
+					else
+					{
+						ban = false;
+						break;
+					}
+		}
+			else
+			{
+				ban = false;
+				break;
+			}
+					
+	}
+
+	return ban;
 	
 }
+
+bool val_iniciar(char* linea)
+{
+	char iniciar[]= "iniciar\n";
+
+	int i=strcmp(iniciar,linea);
+	bool ban;
+	
+	if(i == 0)
+	{
+		ban=true;
+	}
+	
+	return ban;
+}
+
+
+
+bool val_Quees(char* linea)
+{
+	char terminar[]= "terminar";
+	char leer[]="leer";
+	char operacion[] = ":=";
+	char imprimir[]= "imprimir";
+	
+	bool ban= false;
+
+
+
+	if(strstr(linea,leer) != NULL)
+	{
+		ban = val_leer(linea);
+		if(!ban)
+		{
+			printf("Error en instruccion leer, ERROR DE SINTAXIS");
+		}
+
+	}
+	else if(strstr(linea,operacion) != NULL)
+	{
+		ban = true;
+		//val_operacion(linea);
+		if(!ban)
+		{
+			printf("Error en instruccion Oprecion, ERROR DE SINTAXIS");
+		}
+
+	}
+		else if(strstr(linea,imprimir) != NULL)
+		{
+			ban = val_imprimir(linea);
+			if(!ban)
+			{
+				printf("Error en instruccion imprimir, ERROR DE SINTAXIS");
+			}
+
+		}
+			else if(strstr(linea,terminar) != NULL)
+			{
+				ban=val_terminar(linea);
+				if(!ban)
+				{
+				printf("Error en instruccion terminar, ERROR DE SINTAXIS");
+				}
+			}
+				else
+				{
+					ban=false;
+				}
+	return ban;
+}
+
+
+bool val_leer(char* linea1)
+{
+	char leer[]="leer ";
+	int i;
+	int j=0;
+	bool ban = false;
+	
+	for(i=0;i<strlen(linea1);i++)
+	{
+		if(i<=4 && leer[i] == linea1[i])
+		{
+			//printf("\n%c == %c", leer[i], linea[i]);
+			//ban = true;
+		}
+		else if(i>4 && isalpha(linea1[i]) && j==0)
+		{
+			j = 1;
+		}
+			//Si fuera solo para una letra leer i; este else if no va
+			else if((isalpha(linea1[i]) || isdigit(linea1[i])) && j == 1)
+			{
+			}
+				else if(linea1[i] == ';' && linea1[i+1]=='\n')
+				{
+					ban = true;
+					break;	
+				}
+				else 
+				{
+					ban = false;
+					break;
+				}
+	}
+	
+	return ban;
+	
+}
+
+
+
+bool val_imprimir(char* linea1)
+{
+	char imprimir[]= "imprimir ";
+	int i;
+	int j=0;
+	bool ban = false;
+	
+	for(i=0;i<strlen(linea1);i++)
+	{
+		if(i<=8 && imprimir[i] == linea1[i])
+		{
+			//printf("\n%c == %c", leer[i], linea[i]);
+			//ban = true;
+		}
+		else if(i>8 && isalpha(linea1[i]) && j==0)
+		{
+			j = 1;
+		}
+			//Si fuera solo para una letra leer i; este else if no va
+			else if((isalpha(linea1[i]) || isdigit(linea1[i])) && j == 1)
+			{
+			}
+				else if(linea1[i] == ';' && linea1[i+1]=='\n')
+				{
+					ban = true;
+					break;	
+				}
+				else 
+				{
+					ban = false;
+					break;
+				}
+	}
+	
+	return ban;
+}
+
+
+bool val_terminar(char* linea1)
+{
+	char terminar[]= "terminar.\n";
+
+	int i=strcmp(terminar,linea1);
+	bool ban;
+	
+	if(i == 0)
+	{
+		ban=true;
+	}
+	
+	return ban;
+}
+
+
